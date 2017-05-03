b420a456ea8e0a7ee285f35185167cdb50e17d20
Fazle Rahman Ejazi <ahmed.belal98@gmail.com>
2017-05-03 22:10:13 +0530

    Update COINS - Bytelandian gold coins.cpp

--- COINS - Bytelandian gold coins.cpp
+++ COINS - Bytelandian gold coins.cpp
@@ -7,7 +7,8 @@ long long f(int n)
   {
     if(n==0) return 0;
     if(dp[n]!=0) return dp[n];
-    dp[n]=max(n, f(n/2)+f(n/3)+f(n/4));
+    dp[n]=f(n/2)+f(n/3)+f(n/4) ;
+    if(n>dp[n]) dp[n]=n ;
     return dp[n];
   }
 
